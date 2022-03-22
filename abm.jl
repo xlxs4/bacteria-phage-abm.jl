d4a49bc0529b3cc30d164ef8f81edee216bc2589
xlxs4 <orousoultzoglou@gmail.com>
2022-03-22 21:30:33 +0200

    Finalize free phage cycle in step

--- abm.jl
+++ abm.jl
@@ -85,6 +85,18 @@ p_death(a, b, m) = a + ((1 - a) / (1 + exp(-b * (-m))))
 
 p_phage_decay(decay_factor, time_in_state) = 1 - (1 * exp(-decay_factor * time_in_state))
 
+function p_adsorption(cell)
+    nearby_phages = nearby_t(Phage, cell)
+    p_host = model[cell].species === :a ? 0.9 : 0.1 # Tell the compiler that these are all ::Bacterium to help with type inference
+    return p_host / (1 + exp(-length(nearby_phages)))
+end
+
+function p_lysis(phage)
+    nearby_phages = nearby_t(Phage, phage)
+    nearby_phages = isnothing(nearby_phages) ? 0 : length(nearby_phages)
+    return 1 / (1 + model.properties.α * exp(-nearby_phages + model.properties.κ))
+end
+
 function phage_decay(phage)
     if rand() < p_phage_decay(model.decay_factor, model[phage].time_in_state)
         kill_agent!(phage, model)
@@ -93,6 +105,13 @@ end
 
 function tick_phage(phage)
     model[phage].time_in_state += 1
+end
+
+function infect(phage, cell)
+    model[phage].state = :in_host
+    model[phage].time_in_state = 0
+
+    push!(model[cell].phages_inside, phage)
 end
 
 function bacteria_death_inherent(bacteria)
@@ -113,7 +132,7 @@ function bacteria_death_lysis(bacteria)
                 agent = model[phage]
                 if (agent.time_in_state ≥ model.latent_period) && (agent.kind === :virulent || agent.kind === :induced_temperate)
                     if rand() < model.properties.p_burst
-                        println("TODO: burst!")
+                        println("TODO: burst!") #FIXME
                     end
                 end
             end
@@ -145,7 +164,8 @@ function nearby_t(t::DataType, id)
 
     ids = filter(v -> !isempty(v), keep_t_ids.(nearby_pos))
     ids = reduce(vcat, ids)
-    return convert(Vector{Int}, ids[ids.!=nothing])
+    ids = convert(Vector{Int}, ids[ids.!=nothing])
+    return isempty(ids) ? nothing : ids
 end
 
 function add_bacterium_single!(agent::A, model::ABM{<:Agents.DiscreteSpace,A}) where {A<:AbstractAgent}
@@ -183,11 +203,27 @@ function complex_step!(model)
     bacteria_death_inherent(by_single_type(Bacterium)(model))
     bacteria_death_lysis(by_single_type(Bacterium)(model))
 
-    ids = by_single_type(Phage)(model)
-    filter!(id -> model[id].state === :free, ids) # Tell the compiler these are all ::Phage to help with type inference
-    nearby_t.(Ref(Bacterium), ids)
-    tick_phage.(ids)
-    phage_decay.(ids)
+    phages = by_single_type(Phage)(model)
+    filter!(id -> model[id].state === :free, phages) # Tell the compiler these are all ::Phage to help with type inference
+    for phage ∈ phages
+        nearby_cells = nearby_t(Bacterium, phage)
+        isnothing(nearby_cells) && continue
+        target_cell = rand(nearby_cells)
+        if rand() < p_adsorption(target_cell)
+            kind = model[phage].kind
+            if kind === :temperate
+                if rand() < p_lysis(phage)
+                    model[phage].kind = :induced_temperate
+                end
+            end
+            if kind === :virulent || kind === :induced_temperate
+                infect(phage, target_cell)
+            end
+        end
+    end
+    filter!(id -> model[id].state === :free, phages)
+    tick_phage.(phages)
+    phage_decay.(phages)
 
     model.properties.bacteria_count = length(by_single_type(Bacterium)(model))
     model.properties.phages_count = length(by_single_type(Phage)(model))
